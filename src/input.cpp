492891ba879fd83d5f40fa214c9d5506ec6ecd07
Naum Azeredo <naumazeredo@gmail.com>
2020-04-18 00:33:21 -0700

    Cleanup input.cpp

--- src/input.cpp
+++ src/input.cpp
@@ -5,37 +5,17 @@
 #include<iostream>
 
 const Uint8 *state = SDL_GetKeyboardState(NULL);
-Uint8 *old_state;
+Uint8 old_state[SDL_NUM_SCANCODES];
 
-SDL_Scancode used_keys[] = {SDL_SCANCODE_RIGHT,
-                            SDL_SCANCODE_LEFT,
-                            SDL_SCANCODE_DOWN,
-                            SDL_SCANCODE_UP};
-int used_keys_size;
-
-void setup_input() {
-  used_keys_size = sizeof(used_keys)/sizeof(SDL_Scancode);
-
-  int ma = used_keys[0];
-  for(int i = 0; i < used_keys_size; i++) {
-    ma = std::max(ma, (int)used_keys[i]);
-  }
-  old_state = (Uint8 *) malloc(ma+1*sizeof(Uint8));
-}
 void update_keyboard() {
-  for(int i = 0; i < used_keys_size; i++) {
-    old_state[used_keys[i]] = state[used_keys[i]];
-  }
   SDL_PumpEvents();
+
+  for (int code = 0; code < SDL_NUM_SCANCODES; code++) {
+    old_state[code] = state[code];
+  }
 }
 
-bool is_pressed(SDL_Scancode code) {
-  return state[code];
-}
-bool unpress(SDL_Scancode code) {
-  return (old_state[code] and !state[code]);
-}
-bool press(SDL_Scancode code) {
-  return (!old_state[code] and state[code]);
-}
+bool is_pressed (SDL_Scancode code) { return state[code]; }
+bool unpress    (SDL_Scancode code) { return (old_state[code] and !state[code]); }
+bool press      (SDL_Scancode code) { return (!old_state[code] and state[code]); }
 
