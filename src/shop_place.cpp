1fc23a0a73512e3afae3f58c01e4c49353632d6e
Xavier <joaopedroaxavier@gmail.com>
2020-04-20 05:07:20 -0300

    Fix shop collider bug

--- src/shop_place.cpp
+++ src/shop_place.cpp
@@ -12,6 +12,8 @@ void debug_window() {
     int cnt = 0;
     for(auto& shop_place : shop_place_info.shop_places) {
       ImGui::Text("Shop place %d: ", cnt++);
+      ImGui::Text("Shop center: %d %d", shop_place.center.x, shop_place.center.y);
+      ImGui::Text("Player inside: %s", geom::point_inside_convex_polygon(player_info.position, shop_place.collider) ? "YES" : "NO");
       ImGui::Text("State: %s", shop_place.state == ShopPlaceState::FREE ? "FREE" : "OCCUPIED");
       if(shop_place.state == ShopPlaceState::OCCUPIED)
         ImGui::Text("Shop ID: %d", shop_place.shop_id);
@@ -26,14 +28,14 @@ void setup() {
   ShopPlace shop_place;
   shop_place.texture = TextureCode::TEX_BLANK;
   geom::Point trigger_center = { 3.0f * SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2.0f - 100 };
-  geom::Point trigger_h_offset = { 100, 0 };
-  geom::Point trigger_v_offset = { 0, 100 };
+  geom::Point trigger_h_offset = { 200, 0 };
+  geom::Point trigger_v_offset = { 0, 200 };
   shop_place.trigger = { trigger_center - trigger_h_offset - trigger_v_offset,
                          trigger_center + trigger_h_offset - trigger_v_offset,
                          trigger_center + trigger_h_offset + trigger_v_offset,
                          trigger_center - trigger_h_offset + trigger_v_offset};
   geom::Point collider_center = { 3.0f * SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2.0f - 100 };
-  geom::Point collider_h_offset = { 0, 75 };
+  geom::Point collider_h_offset = { 100, 0 };
   geom::Point collider_v_offset = { 0, 75 };
   shop_place.collider = { collider_center - collider_h_offset - collider_v_offset,
                           collider_center + collider_h_offset - collider_v_offset,
@@ -43,6 +45,7 @@ void setup() {
   shop_place.center = collider_center;
   shop_place.state = ShopPlaceState::FREE;
   shop_place_info.shop_places.push_back(shop_place);
+  printf("%d\n", shop_place_info.shop_places.back().trigger.size());
 }
 
 void render() {
