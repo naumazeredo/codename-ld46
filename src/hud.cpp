e9791739df35ad05432800d39cb2acf13d8c5ea2
Xavier <joaopedroaxavier@gmail.com>
2020-04-20 21:13:27 -0300

    fix coins z position

--- src/hud.cpp
+++ src/hud.cpp
@@ -12,34 +12,34 @@ const int DIGIT_HEIGHT = 15;
 void display_digit(int x, int y, int digit) {
     switch (digit) {
         case 0:
-            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_0, 9999);
+            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_0, -3e5);
             break;
         case 1:
-            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_1, 9999);
+            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_1, -3e5);
             break;
         case 2:
-            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_2, 9999);
+            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_2, -3e5);
             break;
         case 3:
-            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_3, 9999);
+            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_3, -3e5);
             break;
         case 4:
-            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_4, 9999);
+            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_4, -3e5);
             break;
         case 5:
-            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_5, 9999);
+            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_5, -3e5);
             break;
         case 6:
-            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_6, 9999);
+            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_6, -3e5);
             break;
         case 7:
-            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_7, 9999);
+            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_7, -3e5);
             break;
         case 8:
-            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_8, 9999);
+            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_8, -3e5);
             break;
         case 9:
-            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_9, 9999);
+            render::add_to_render(x, y, DIGIT_WIDTH, DIGIT_HEIGHT, TextureCode::TEX_DIGIT_9, -3e5);
             break;
         default:
             break;
@@ -65,8 +65,8 @@ void update() {
   int coins_x = SCREEN_WIDTH - coins_width;
   int coins_y = SCREEN_HEIGHT - coins_height;
 
-  render::add_to_render(health_x, health_y, health_width, health_height, TextureCode::TEX_HUD_HEALTH, 9999);
-  render::add_to_render(coins_x, coins_y, coins_width, coins_height, TextureCode::TEX_HUD_COINS, 9999);
+  render::add_to_render(health_x, health_y, health_width, health_height, TextureCode::TEX_HUD_HEALTH, -3e5);
+  render::add_to_render(coins_x, coins_y, coins_width, coins_height, TextureCode::TEX_HUD_COINS, -2e5);
   display_number(SCREEN_WIDTH - 40, SCREEN_HEIGHT - 28, player_info.money);
 }
 
